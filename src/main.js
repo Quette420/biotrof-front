a3a76b0ca542f1c520ac5aa604642863fa6c50fe
George <gsaranov@gmail.com>
2023-02-18 12:53:34 +0300

    lesson 4 complete

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router/router'
 import store from './store/store'
+import dateFilter from './filters/date.filter'
+import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min.js'
 
 Vue.config.productionTip = false
+
+Vue.filter('date', dateFilter)
 
 new Vue({
   router,
