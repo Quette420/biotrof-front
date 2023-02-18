504abcd7f95a753691fe7e3d6876baa7774d5ef8
George <gsaranov@gmail.com>
2023-02-18 10:14:52 +0300

    modified:   package-lock.json

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +7,56 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    meta: {layout: 'main'},
+    component: () => import('../views/HomeView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import('../views/MyLogin.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('../views/MyRegistration.vue')
+  },
+  {
+    path: '/detail/:id',
+    name: 'detail',
+    meta: {layout: 'main'},
+    component: () => import('../views/DetailRecord.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('../views/MyPlanning.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('../views/MyRecord.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import('../views/MyCategories.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('../views/MyHistory.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('../views/MyProfile.vue')
   }
 ]
 
