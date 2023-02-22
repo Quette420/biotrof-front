efd0710986fec61a03cbb7634ed5dd7cc2c2d38c
George <gsaranov@gmail.com>
2023-02-22 22:08:54 +0300

    modified:   src/views/BarChart.vue 	deleted:    src/views/MyPlanning.vue

--- src/views/BarChart.vue
+++ src/views/BarChart.vue
@@ -2,7 +2,18 @@
     <div id="bar-chart">
         <div>
         <canvas id="myChart" width="20"></canvas>
-        <canvas id="myChart2"></canvas>
+        <button class="btn-small btn" 
+            @click="calculateDiagramm">
+              <i class="material-icons"></i>
+            </button>
+            <button class="btn-small btn" 
+            @click="calculateDiagramm2">
+              <i class="material-icons"></i>
+            </button>
+            <button class="btn-small btn" 
+            @click="calculateDiagramm3">
+              <i class="material-icons"></i>
+            </button>
     </div>
     </div>
 </template>
@@ -10,15 +21,13 @@
 <script>
 import Chart from 'chart.js/auto'
 export default {
-    mounted() {
-        const ctx = document.getElementById('myChart')
-    // eslint-disable-next-line
-        const myChart = new Chart(ctx, {
-            type: 'line',
+    data() {
+        return{
+           
             data:{
                 labels: ['Red','Blue','Yellow','Green','Purple','Orange'],
                 datasets: [{
-                label: 'Голоса',
+                label: 'Test',
                 data: [12,19,3,5,2,3],
                 backgroundColor: [
                     'rgba(255,99,132,0.2)',
@@ -36,7 +45,7 @@ export default {
                     'rgba(153,102,255,1)',
                     'rgba(255,159,64,1)'
                 ],
-                borderWidth: 5
+                borderWidth: 2
                 }]
             },
             options: {
@@ -45,44 +54,47 @@ export default {
                         beginAtZero: true
                     }
                 }
-            }
-        })
-        const ctx2 = document.getElementById('myChart2')
+            },
+            myChart: ''
+        }
+        
+    },
+    mounted() {
+        const ctx = document.getElementById('myChart')
     // eslint-disable-next-line
-        const myChart2 = new Chart(ctx2, {
+        this.myChart = new Chart(ctx, {
             type: 'line',
-            data:{
-                labels: ['','','','','',''],
-                datasets: [{
-                label: 'Голоса',
-                data: [8,11,3,21,9,11],
-                backgroundColor: [
-                    'rgba(255,99,132,0.2)',
-                    'rgba(54,162,235,0.2)',
-                    'rgba(255,206,86,0.2)',
-                    'rgba(75,192,192,0.2)',
-                    'rgba(153,102,255,0.2)',
-                    'rgba(255,159,64,0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54,162,235,1)',
-                    'rgba(255,206,86,1)',
-                    'rgba(75,192,192,1)',
-                    'rgba(153,102,255,1)',
-                    'rgba(255,159,64,1)'
-                ],
-                borderWidth: 5
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
+            data: this.data,
+            options: this.options
         })
+       
+}, methods: {
+    calculateDiagramm () {
+        console.log()
+        this.data.labels = ['Ожидает оплаты','На подписании','Изготовление','Готово к отгрузке','Отгрузка','Отгружено']
+        this.data.datasets[0].label = 'Статистика по заказам'
+        this.data.datasets[0].data[0] = 30
+        this.data.datasets[0].data[1] = 2
+        this.data.datasets[0].data[3] = 5
+        this.data.datasets[0].data[4] = 22
+        this.data.datasets[0].data[5] = 9
+        this.data.datasets[0].data[6] = 15
+        this.myChart.update()
+    },
+    calculateDiagramm2 () {
+        console.log()
+        this.data.labels = ['Январь','Февраль','Апрель','Май','Июнь','Июль', 'Август','Сентябрь','Октябрь','Ноябрь','Декабрь']
+        this.data.datasets[0].label = 'Статистика за год'
+        this.data.datasets[0].data = [100, 120, 130, 140, 150, 160, 130, 100, 120, 130, 140, 150, 160]
+        this.myChart.update()
+    },
+    calculateDiagramm3 () {
+        console.log()
+        this.data.labels = ['1','2','3','4','5','6', '7','8','9','10','11','12','13','14','15','16','17', '18','19','20','21','22','23','24','25','26','27','28','29','30','31']
+        this.data.datasets[0].label = 'Статистика за месяц'
+        this.data.datasets[0].data = [100, 120, 130, 140, 150, 160, 130, 100, 120, 130, 110, 190, 50, 75, 170, 66, 99, 200, 100, 120, 130, 140, 190, 110, 120, 100, 100, 120, 130, 140, 150]
+        this.myChart.update()
+    }
 }
 }
 </script>
@@ -96,17 +108,8 @@ export default {
 }
     canvas#myChart{
 
-    width:500px !important;
-    height:300px !important;
   
 
 }
-canvas#myChart2{
 
-width:500px !important;
-height:400px !important;
-margin-left: 25%;
-margin-top: 0%;
-width: 1em;
-}
 </style>
